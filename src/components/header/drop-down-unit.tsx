3314cf04d3015dffb9cd990b99e1bb3d67d199f6
Davichobits <davidruizdeveloper@gmail.com>
2025-09-05 21:08:24 -0500

    Refactor drop-dow-unit

--- src/components/header/drop-down-unit.tsx
+++ src/components/header/drop-down-unit.tsx
@@ -10,31 +10,26 @@ export const DropDownUnit = ({unit, indicator}: Props) => {
 
   const store = useIndicatorStore((state)=> state);
 
-  let indicatorUnit: UnitTemperature | UnitWindSpeed | UnitPrecipitation
-
-  if(indicator === 'Temperature'){
-    indicatorUnit = store.temperatureUnit;
-  }else if(indicator === 'Wind Speed'){
-    indicatorUnit = store.windSpeedUnit
-  }else{
-    indicatorUnit = store.precipitationUnit
+  const indicatorMap = {
+    Temperature: {
+      value: store.temperatureUnit as UnitTemperature,
+      toggle: store.toggleTemperatureUnit,
+    },
+    'Wind Speed': {
+      value: store.windSpeedUnit as UnitWindSpeed,
+      toggle: store.toggleWindSpeedUnit,
+    },
+    Precipitation: {
+      value: store.precipitationUnit as UnitPrecipitation,
+      toggle: store.togglePrecipitationUnit,
+    },
   }
 
-  const isSelected = indicatorUnit === unit;
-
-  const handleClick = () => {
-    if(indicator === 'Temperature'){
-      store.toggleTemperatureUnit();
-    }else if(indicator === 'Wind Speed'){
-      store.toggleWindSpeedUnit();
-    }else{
-      store.togglePrecipitationUnit();
-    }
-    
-  }
+  const { value, toggle } = indicatorMap[indicator]
+  const isSelected = value === unit
 
   return (
-    <button onClick={handleClick} className={`rounded-lg px-2 py-2.5 flex justify-between cursor-pointer ${isSelected && 'bg-Neutral-700' } hover:bg-Neutral-700 transition-colors`}>
+    <button onClick={toggle} className={`rounded-lg px-2 py-2.5 flex justify-between cursor-pointer ${isSelected && 'bg-Neutral-700' } hover:bg-Neutral-700 transition-colors`}>
       <p>{unit}</p>
       {
         isSelected && (<Image width={14} height={14} src={'/images/icon-checkmark.svg'} alt='icon checkmark' />)
