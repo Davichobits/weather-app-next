1cdecc814a183bce0fcb6889a13a7fd41ca6cb48
Davichobits <davidruizdeveloper@gmail.com>
2025-09-22 15:19:43 -0500

    fix: hidration error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,14 @@ import { IndicatorStoreProvider } from '@/stores/indicator-store-provider';
 export default function Home() {
   return (
     <IndicatorStoreProvider>
-      <div className='m-4 md:min-w-[720px]'>
+      <main className='m-4 md:min-w-[720px]'>
         <Header />
         <Hero />
         <Search />
         <WeatherInfo />
         <DailyForecast />
         <HourlyForecast />
-      </div>
+      </main>
     </IndicatorStoreProvider>
   );
 }
